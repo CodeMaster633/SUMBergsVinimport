ca6ad12e8f1a35c36aa0d2ad584ca67d9f67e7db
CodeMaster633 <126244327+CodeMaster633@users.noreply.github.com>
2024-11-06 15:02:41 +0100

    fix

---
+++ DTO/Model/Lager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Model
+{
+    public class Lager
+    {
+        public Lager() { }
+
+        public Lager(string navn, string adresse, string kontaktperson)
+        {
+            Navn = navn;
+            Adresse = adresse;
+            Kontaktperson = kontaktperson;
+        }
+
+        public string Navn { get; set; }
+        public string Adresse { get; set; }
+        public string Kontaktperson { get; set; }
+    }
+}
