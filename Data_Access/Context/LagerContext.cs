14346b53e2f2d4b66e5b756c729c5971e3342ed1
MikkelBangK <mikkelbang94@outlook.com>
2024-11-25 11:32:17 +0100

    Det sidste fra mig dvd g

--- Data_Access/Context/LagerContext.cs
+++ Data_Access/Context/LagerContext.cs
@@ -18,9 +18,9 @@ namespace Data_Access.Context
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=LAPTOP-TT7JTDJT\\SQLEXPRESS;Initial Catalog=Lagre;Integrated Security = SSPI; TrustServerCertificate=true");
-            //optionsBuilder.UseSqlServer("Data Source=LAPTOP-CP8PKIBC\\SQLEXPRESS;Initial Catalog=Lager;Integrated Security=True; TrustServerCertificate=true");
-            //optionsBuilder.UseSqlServer("Server=localhost,1433;Database=Lagre;User Id=sa;Password=reallyStrongPwd123;TrustServerCertificate=true");
+            //optionsBuilder.UseSqlServer("Data Source=LAPTOP-TT7JTDJT\\SQLEXPRESS;Initial Catalog=Lagre;Integrated Security = SSPI; TrustServerCertificate=true");
+            //optionsBuilder.UseSqlServer("Data Source=LAPTOP-CP8PKIBC\\SQLEXPRESS;Initial Catalog=Lagre;Integrated Security=True; TrustServerCertificate=true");
+            optionsBuilder.UseSqlServer("Server=localhost,1433;Database=Lagre;User Id=sa;Password=reallyStrongPwd123;TrustServerCertificate=true");
             //optionsBuilder.UseSqlServer("Data Source=MRCARLSEN\\SQLEXPRESS;Initial Catalog=Lagre;Integrated Security = SSPI; TrustServerCertificate=true");
             optionsBuilder.LogTo(message => Debug.WriteLine(message));
             
--- Data_Access/Model/TjekkedeProdukter.cs
+++ Data_Access/Model/TjekkedeProdukter.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Data_Access.Model;
 
 public class TjekkedeProdukter
 {
+    [Key]
     public Guid TjekId { get; set; }
-    public int Id { get; set; }
+    
+    [ForeignKey("Produkt")]
+    public int ProduktId { get; set; }
+    public Produkt? Produkt { get; set; }
     public string? Navn { get; set; }
     public int Antal { get; set; }
+    public int LagerId { get; set; }
     public DateTime Udloebsdato { get; set; }
     public DateTime TjekketDato { get; set; }
     public string VisningsTekst => $"{Navn} - {Udloebsdato}";
